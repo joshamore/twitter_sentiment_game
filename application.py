504261aca946128feca63a6f4726aee438e07f2f
Josh Amore <joshamore31@gmail.com>
2017-10-20 06:06:22 +1100

    moved helper functions/classes to helpers.py file

--- application.py
+++ application.py
@@ -1,47 +1,17 @@
-from twython import Twython
-from twython import TwythonStreamer
 from textblob import TextBlob
+from Flask import Flask
+from helpers import *
 
-# Don't push with keys!
-consumer_key = 'and'
-consumer_secret = 'and'
-access_token = 'and'
-access_secret = 'and'
+app = Flask(__name__)
 
-# Stores stream tweet data (including user)
-streamTweet = []
+@app.route('/')
+def hello_world():
+    return 'Hello, world'
 
-# Setting up streamer
-# Docs: http://twython.readthedocs.io/en/latest/usage/streaming_api.html?highlight=track#filtering-public-statuses
-class MyStreamer(TwythonStreamer):
-    
-    def on_success(self, data):
-        # Storing tweet if has a display picture and at least 100 tweets
-        if data['user']['default_profile_image'] == False and data['user']["statuses_count"] > 100:
-            streamTweet.append(data)
-            self.disconnect()
-    def on_error(self, status_code, data):
-        print(status_code)
-    
-        # Stops trying to get data on error
-        self.disconnect()
 
 if __name__ == '__main__':
-    # Setting auths
-    twitter = Twython(consumer_key, consumer_secret, access_token, access_secret)
-    stream = MyStreamer(consumer_key, consumer_secret, access_token, access_secret)
+    guessTweetData = pullTweets()
     
-    # Filters tweets checking for 10 most common english words
-    stream.statuses.filter(track=['the', 'of', 'to', 'and', 'in', 'you', 'that', 'it', 'is', 'for'])
-    
-    #Storing tweets text
-    guessTweetData = []
-    
-    # Pulling in user tweets
-    user_timeline = twitter.get_user_timeline(user_id=streamTweet[0]['user']['id'], count=50)
-    for tweet in user_timeline:
-        guessTweetData.append(tweet['text'])
-        
     # Checking the polarity of each tweet
     polarityCombo = 0
     for tweet in guessTweetData:
---
+++ helpers.py
@@ -0,0 +1,44 @@
+from twython import Twython
+from twython import TwythonStreamer
+
+# Don't push with keys!
+consumer_key = 'and'
+consumer_secret = 'and'
+access_token = 'and'
+access_secret = 'and'
+
+# Stores stream tweet data (including user)
+streamTweet = []
+
+# Setting up streamer
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        # Storing tweet if has a display picture and at least 100 tweets
+        if data['user']['default_profile_image'] == False and data['user']["statuses_count"] > 100:
+            streamTweet.append(data)
+            self.disconnect()
+            
+    def on_error(self, status_code, data):
+        # Prints status code and stops trying to get data on error
+        print(status_code)
+        self.disconnect()
+
+# Will pull in 50 most recent tweets from a random user
+def pullTweets():
+    # Setting auths
+    twitter = Twython(consumer_key, consumer_secret, access_token, access_secret)
+    stream = MyStreamer(consumer_key, consumer_secret, access_token, access_secret)
+    
+    # Filters tweets checking for 10 most common english words
+    stream.statuses.filter(track=['the', 'of', 'to', 'and', 'in', 'you', 'that', 'it', 'is', 'for'])
+    
+    #Storing tweets text
+    guessTweetData = []
+    
+    # Pulling in user tweets
+    user_timeline = twitter.get_user_timeline(user_id=streamTweet[0]['user']['id'], count=50)
+    for tweet in user_timeline:
+        guessTweetData.append(tweet['text'])
+        
+    # Returning text of past 50 tweets in a list
+    return guessTweetData
