0e24b8526b17d515fa9279e00372181d852cdee7
Josh Amore <joshamore31@gmail.com>
2017-10-20 07:22:09 +1100

    added functions/routes to index and register pages (with placehold content) and added placeholder templates for both

--- application.py
+++ application.py
@@ -1,15 +1,23 @@
 from textblob import TextBlob
-from Flask import Flask
+import os
+from flask import Flask, session, render_template
 from helpers import *
 
 app = Flask(__name__)
 
-@app.route('/')
+# Home page
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, world'
+    return render_template('index.html')
 
+# Register account page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
+    #!! TODO !!
+    # Move this functionality into helpers.py
     guessTweetData = pullTweets()
     
     # Checking the polarity of each tweet
